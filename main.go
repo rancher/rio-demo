f8fab97fddc8ed5e98e45cd6373ad6feff3197f9
Darren Shepherd <darren@rancher.com>
2019-07-24 14:34:34 -0700

    Test commit

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
-	time.Sleep(100 * time.Millisecond)
+	time.Sleep(101 * time.Millisecond)
 	fmt.Fprintln(w, "Hi there, I'm running in Rio")
 }
 
