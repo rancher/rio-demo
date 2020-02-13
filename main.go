7e28d913f8eb30ccad498d1b497e0a3887e48afd
SMY ALTAMASH <30286162+SMYALTAMASH@users.noreply.github.com>
2020-02-13 15:43:31 +0530

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I'm the master")
+	fmt.Fprintln(w, "The King Never Fails To win His Destiny")
 }
 
 func main() {
