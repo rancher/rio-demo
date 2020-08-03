9a529d238bd626498880137baec5473be2bd6742
David Ko <dko@suse.com>
2020-08-03 14:50:20 +0800

    update

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 	// Arbitrary sleep so that we can demonstrate autoscaler
 	time.Sleep(101 * time.Millisecond)
-	fmt.Fprintln(w, "Hi there, I'm running in Rio")
+	fmt.Fprintln(w, "Hi there, I'm running in Rio - SUSE")
 }
 
 func main() {
